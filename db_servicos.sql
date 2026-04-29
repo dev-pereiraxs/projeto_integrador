9af97b898d749f94d74ff4540c9346fa8e0aa40e
LUCAS DE OLIVEIRA PINTO <lucas.opinto3@senacsp.edu.br>
2026-04-29 20:25:40 -0300

    alteração no txt

--- db_servicos.sql
+++ db_servicos.sql
@@ -16,3 +16,41 @@ drop table cadastro_clientes;
 ALTER TABLE cadastro_clientes CHANGE idade data_nascimento DATE;
 ALTER TABLE cadastro_clientes ADD sexo VARCHAR(20);;
 
+
+
+
+
+==============================================================
+
+
+CREATE DATABASE IF NOT EXISTS servicos;
+USE servicos;
+
+CREATE TABLE IF NOT EXISTS cadastro_clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL,
+    idade INT NULL, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NULL
+);
+
+select * from cadastro_clientes;
+drop table cadastro_clientes;
+
+ALTER TABLE cadastro_clientes CHANGE idade data_nascimento DATE;
+ALTER TABLE cadastro_clientes ADD sexo VARCHAR(20);
+
+
+CREATE TABLE cadastro_prestadores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    sexo VARCHAR(20) NOT NULL,
+    areas_atuacao TEXT NOT NULL
+);
+
+select * from cadastro_prestadores;
