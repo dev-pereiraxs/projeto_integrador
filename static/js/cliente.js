f15a2fecbfc0ee6903a3fa82d8f7eb7e34f755e2
dev-pereiraxs <andrezinnyt08@gmail.com>
2026-04-30 22:08:24 -0300

    alteração no projeto

--- static/js/cliente.js
+++
@@ -1,9 +0,0 @@
-<script>
-    const cards = document.querySelectorAll('.preference-card');
-
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            card.classList.toggle('active');
-        });
-    });
-</script>
