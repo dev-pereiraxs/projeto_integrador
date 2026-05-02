40326a6bb08b2a02f8bfdbc76b4993885b3fdcb2
Felipe Pereira <fsantosaguiarpereira1@gmail.com>
2026-05-02 20:10:23 -0300

    removendo credenciais

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = "2a962fb071252f38d97cafb2f3a84c80c49568ebb87bc1b1"
 
-CLIENT_ID     = "GOOGLE_CLIENT_ID_REMOVIDO"
-CLIENT_SECRET = "GOOGLE_CLIENT_SECRET_REMOVIDO"
+import os
+
+CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+
 
 # =========================
 # GOOGLE AUTH
